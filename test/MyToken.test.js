b2004175531ea0e9a33af0a9442cf24a602dee64
akhilmantha <akhilmantha@live.com>
2022-04-11 11:59:16 +0530

    test: Adding host and network_id to config
    Changes in config and fixing issues in test file

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -1,6 +1,5 @@
 const Token = artifacts.require("MyToken");
 
-const { assert } = require("chai");
 var chai = require("chai");
 const BN = web3.utils.BN;
 const chaiBN = require("chai-bn")(BN);
@@ -11,13 +10,13 @@ chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
-contract("Token Test", async (accounts)) => {
+contract("Token Test", async (accounts) => {
     it("all tokens should be in my account", async () => {
-        let instance = await Token.deplyed();
+        let instance = await Token.deployed();
         let totalSupply = await instance.totalSupply();
         //let balance  = await instance.balanceOf(accounts[0]);
         //assert.equal(balance.valueOf(), initialSupply.valueOf(), "The balance was not the same");
         expect(await instance.balanceOf(accounts[0])).to.be.a.bignumber.equal(totalSupply);
 
     })
-} 
+}); 
