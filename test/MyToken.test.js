6bea144e70a9dc71e89983ad6e42935934034221
akhilmantha <akhilmantha@live.com>
2022-04-11 16:31:04 +0530

    test: Adding checks for limit on transfer of tokens wrt deployers token amount.

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -17,11 +17,13 @@ contract("Token Test", async (accounts) => {
     it("all tokens should be in my account", async () => {
         let instance = await Token.deployed();
         let totalSupply = await instance.totalSupply();
+        //Old format:
         //let balance  = await instance.balanceOf(accounts[0]);
         //assert.equal(balance.valueOf(), initialSupply.valueOf(), "The balance was not the same");
+        //Readable format:
         expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
 
-    })
+    });
 
     it("is possible to send tokens between accounts", async () => {
         const sendTokens = 1;
@@ -31,5 +33,12 @@ contract("Token Test", async (accounts) => {
         expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
         expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
         expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens)); 
-    })
+    });
+
+    it("is not possible to send more tokens than available in total", async () => {
+        let instance = await Token.deployed();
+        let balanceOfDeployer = await instance.balanceOf(deployerAccount);
+        expect(instance.transfer(recipient, new BN(balanceOfDeployer+1))).to.be.rejected;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
+    });
 }); 
