85f07c4bf3876dd5652a0bf1e27a7e514cc6db58
akhilmantha <akhilmantha@live.com>
2022-04-12 20:34:48 +0530

    chore: Removing hardcoded values from test and migrations file.

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -15,7 +15,7 @@ contract("Token Test", async (accounts) => {
     const [deployerAccount, recipient, anotherAccount] = accounts
 
     beforeEach(async() => {
-        this.myToken = await Token.new(1000000000);
+        this.myToken = await Token.new(process.env.INITIAL_TOKENS);
     })
 
     it("all tokens should be in my account", async () => {
