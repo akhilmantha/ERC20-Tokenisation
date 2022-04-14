284e028bc181a9943561e1840d09c2452eeb58cb
akhilmantha <akhilmantha@live.com>
2022-04-14 12:21:04 +0530

    chore: Minor changes.
    - Removing placeholders in app.js
    - Change in the name of the token

--- client/src/App.js
+++ client/src/App.js
@@ -52,17 +52,10 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Cold Brew Coffee Works</h1>
+        <p>Get you Tokens today!</p>
+        <h2>KYC Whitelisting</h2>
+        Address to allow: <input type= "text" name = "kycAddress" value = {this.state.kycAddress} />
       </div>
     );
   }
