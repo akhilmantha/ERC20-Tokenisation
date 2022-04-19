96ae9b5a287fb41b3796940a7ff9a76619669087
akhilmantha <akhilmantha@live.com>
2022-04-19 14:54:31 +0530

    chore: Running truffle migrate after changes in config
    - Adding hdwallet provider node module
    - Adding hdwallet provider in truffle_config
    - Re-compiling and re-deploying contracts
    - Resetting migrations after changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 require("dotenv").config({path: "./.env"});
-//const HDWalletProvider = require("@truffle/hdwallet-provider");
-//const AccountIndex = 0;
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const AccountIndex = 1;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -11,6 +11,12 @@ module.exports = {
     development: {
       port: 7545,
       host: "127.0.0.1",
+      network_id: 1337
+    },
+    ganache_local: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", AccountIndex)
+      },
       network_id: 1337
     }
   },
